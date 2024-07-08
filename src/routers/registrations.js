99afa35e342e08ff7c5d48f5302de38f983c04ac
Tim Zoltie <t.zoltie@gmail.com>
2024-07-08 11:10:49 +0100

    add POST/register user created

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -7,13 +7,21 @@ const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
     // Get the username and password from request body
+    const { username, password } = req.body
     
     // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
+    const hshPassword = await bcrypt.hash(password, 8)
     // Save the user using the prisma user model, setting their password to the hashed version
-    
+    const createdUser = await prisma.user.create({
+        data: {
+            username: username,
+            password: hshPassword
+        }
+    })
+    const {id} = createdUser
+    const response = {id, username}
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res.status(201).json({ user: response })
 });
 
 module.exports = router;
